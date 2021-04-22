0c36fce42d99b90240ecfc4a9aa5f5160a43c6f0
Lenka Ruzickova <lenka.hrabakova@gmail.com>
2021-04-22 14:40:52 +0200

    ukol 5 bonus

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let playing = 'circle';
 
 let click = 0;
 const playingCross = document.querySelector('.types');
+const boardSize = 10; // 10x10
+const fields = document.querySelectorAll('.play button');
 
 const playingFunction = (event) => {
   if (event.target.disabled) {
@@ -20,18 +22,19 @@ const playingFunction = (event) => {
   event.target.disabled = true;
 
   if (isWinningMove(event.target)) {
-    alert(`Vyhrál ${getSymbol(event.target)}`);
+    // alert(`Vyhrál ${getSymbol(event.target)}`);
+
+    if (confirm(`Vyhrál ${getSymbol(event.target)}. Spustit novou hru?`)) {
+      location.reload();
+    }
   }
 };
 
-document.querySelectorAll('.play button').forEach((element) => {
+fields.forEach((element) => {
   element.addEventListener('click', playingFunction);
 });
 
 //check who wins
-
-const boardSize = 10; // 10x10
-const fields = document.querySelectorAll('.play button');
 
 const getPosition = (field) => {
   let fieldIndex = 0;
@@ -49,7 +52,7 @@ const getPosition = (field) => {
 };
 
 const getField = (row, column) => {
-  return document.querySelectorAll('.play button')[row * 10 + column];
+  return fields[row * 10 + column];
 };
 
 const getSymbol = (field) => {
@@ -110,6 +113,60 @@ const isWinningMove = (field) => {
   }
 
   if (inColumn >= symbolsToWin) {
+    return true;
+  }
+
+  let inDiagonalA = 1;
+
+  // kouka doprava dolu
+  i = origin.row;
+  let x = origin.column;
+
+  while (
+    i > boardSize - 1 &&
+    x > -1 &&
+    symbol === getSymbol(getField(i + 1, x + 1))
+  ) {
+    inDiagonalA++;
+    i++;
+    x++;
+  }
+
+  //kouka doleva nahoru
+  i = origin.row; //i je row
+  x = origin.column; //x column
+
+  while (i > 0 && x > 0 && symbol === getSymbol(getField(i - 1, x - 1))) {
+    inDiagonalA++;
+    x--;
+    i--;
+  }
+  if (inDiagonalA >= symbolsToWin) {
+    return true;
+  }
+
+  let inDiagonalB = 1;
+  //kouka doprava nahoru
+  i = origin.row;
+  x = origin.column;
+
+  while (i > 0 && x > 0 && getSymbol(getField(i - 1, x + 1))) {
+    inDiagonalB++;
+    x++;
+    i--;
+  }
+
+  //kouka doleva dolu
+  i = origin.row;
+  x = origin.column;
+
+  while (i > boardSize - 1 && x > -1 && getSymbol(getField(i + 1, x - 1))) {
+    inDiagonalB++;
+    x--;
+    i++;
+  }
+
+  if (inDiagonalB >= symbolsToWin) {
     return true;
   }
 
